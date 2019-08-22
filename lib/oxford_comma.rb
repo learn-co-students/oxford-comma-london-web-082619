e9f1d88d8d3b93c3d536b4b4e776b453921e651b
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-08-22 16:53:32 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 2
+  array[-2] << " and "
+  array.join
+  elsif array.length > 2
+    array[-1].prepend "and "
+	array.join(", ")
+	else array.join
+  end
 end
